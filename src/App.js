506d0e8a2f755bc249186c83a009233879e0bcaf
melissa hale <melissa.hale@q2.com>
2021-08-22 09:47:04 -0500

    push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg">
+        <p className="content">Coming Soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
